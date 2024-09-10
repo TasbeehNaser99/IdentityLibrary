7e37b29f45e89da926908a660cc516a5baa7fc0b
TasbeehNaser <tasbeehnaser2002@gmail.com>
2024-09-11 00:41:43 +0300

    Login system

--- Identity/Controllers/AccountsController.cs
+++ Identity/Controllers/AccountsController.cs
@@ -9,11 +9,13 @@ namespace Identity.Controllers
     {
         private readonly ApplicationDbContext context;
         private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public AccountsController(ApplicationDbContext _context,UserManager<IdentityUser> _userManager)
+        public AccountsController(ApplicationDbContext _context,UserManager<IdentityUser> _userManager,SignInManager<IdentityUser>_signInManager)
         {
             context = _context;
             userManager = _userManager;
+            signInManager = _signInManager;
         }
         public IActionResult Register()
         {
@@ -36,7 +38,17 @@ namespace Identity.Controllers
             return View(model);
         }
         public IActionResult Login() { 
-            return Content("login page"); 
+            return View(); 
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,false);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
         }
     }
 }
---
+++ Identity/Models/ViewModel/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.ViewModel
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [MaxLength(40, ErrorMessage = "Email cannot be longer than 40 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}

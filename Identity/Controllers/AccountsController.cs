e1536370af98e420dbbacd61183f5343773fb08b
TasbeehNaser <tasbeehnaser2002@gmail.com>
2024-09-11 00:03:33 +0300

    create register system

---
+++ Identity/Controllers/AccountsController.cs
@@ -0,0 +1,42 @@
+﻿using Identity.Data;
+using Identity.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountsController(ApplicationDbContext _context,UserManager<IdentityUser> _userManager)
+        {
+            context = _context;
+            userManager = _userManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                PhoneNumber = model.phone
+            };
+         var result=  await userManager.CreateAsync(user,model.Password);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+        public IActionResult Login() { 
+            return Content("login page"); 
+        }
+    }
+}
---
+++ Identity/Models/ViewModel/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.ViewModel
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [MaxLength(40, ErrorMessage = "Email cannot be longer than 40 characters.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        public string phone { get; set; }
+
+    }
+}
